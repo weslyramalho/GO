5b8d5cca4199ba20f448dbca33105b5c925a9fbd
weslyramalho <weslyramalho@yahoo.com.br>
2023-05-04 17:42:52 -0300

    teste

---
+++ testes/testing/repository_mock.go
@@ -0,0 +1,13 @@
+package tax
+
+import (
+	"github.com/stretchr/testify/mock"
+)
+
+type TaxRepositoryMock struct {
+	mock.Mock
+}
+
+func (m *TaxRepositoryMock) SaveTax(amount float64) error {
+	args := m.Called(tax)
+}
