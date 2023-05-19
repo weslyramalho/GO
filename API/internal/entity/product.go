f14f05ed3ce6015566f6eb58150988fce23bda3d
weslyramalho <weslyramalho@yahoo.com.br>
2023-05-19 16:43:33 -0300

    test user ok

--- API/internal/entity/product.go
+++ API/internal/entity/product.go
@@ -2,6 +2,7 @@ package entity
 
 import (
 	"errors"
+	"time"
 
 	"github.com/weslyramalho/GO/tree/main/API/pkg/entity"
 )
@@ -18,14 +19,14 @@ type Product struct {
 	ID        entity.ID `json:"id"`
 	Name      string    `json:"name"`
 	Price     int       `json:"price"`
-	CreatedAt time.Time    `json:"created_at"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
-func NewProduct(name string, price int)(*Product, error){
+func NewProduct(name string, price int) (*Product, error) {
 	product := &Product{
-		ID: entity.NewID(),
-		Name: name,
-		Price: price,
+		ID:        entity.NewID(),
+		Name:      name,
+		Price:     price,
 		CreatedAt: time.Now(),
 	}
 	err := product.Validate()
@@ -36,22 +37,21 @@ func NewProduct(name string, price int)(*Product, error){
 }
 
 func (p *Product) Validate() error {
-	if p.ID.String() == ""{
+	if p.ID.String() == "" {
 		return ErrIDIsRequired
 	}
 
-	if err:= entity.ParseID(string(p.ID.String()); err != nil){
-		return ErrIvalidID	
+	if _, err := entity.ParseID(p.ID.String()); err != nil {
+		return ErrIvalidID
 	}
-	if p.Name == ""{
+	if p.Name == "" {
 		return ErrNameIsRequired
 	}
-	if p.Price == 0{
+	if p.Price == 0 {
 		return ErrPriceIsRequired
 	}
-	if p.Price < 0{
+	if p.Price < 0 {
 		return ErrInvalidPrice
 	}
 	return nil
-	}
-
+}
--- API/internal/infra/database/user_db_test.go
+++ API/internal/infra/database/user_db_test.go
@@ -32,3 +32,22 @@ func TestCreateUser(t *testing.T) {
 	assert.NotNil(t, userFound.Password)
 
 }
+
+func TestFindByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.User{})
+	user, _ := entity.NewUser("jon", "j@j", "1234565")
+	userDB := NewUser(db)
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	userFound, err := userDB.FindByEmail(user.Email)
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotNil(t, userFound.Password)
+}
