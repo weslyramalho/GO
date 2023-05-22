b8faba4f909aaf8760384a0291ddd48929845550
weslyramalho <weslyramalho@yahoo.com.br>
2023-05-22 19:56:01 -0300

    Findal product

--- API/internal/infra/database/product_db.go
+++ API/internal/infra/database/product_db.go
@@ -38,3 +38,16 @@ func (p *Product) Delete(id string) error {
 	}
 	return p.DB.Delete(product).Error
 }
+func (p *Product) FindAll(page, limit int, sort string) ([]entity.Product, error) {
+	var products []entity.Product
+	var err error
+	if sort != "" && sort != "asc" && sort != "desc" {
+		sort = "asc"
+	}
+	if page != 0 && limit != 0 {
+		err = p.DB.Limit(limit).Offset((page - 1) * limit).Order("Created_at " + sort).Find(&products).Error
+	} else {
+		err = p.DB.Order("Created_at " + sort).Find(&products).Error
+	}
+	return products, err
+}
