7c2d52483adf8ae64395b831c66c1c125bedb44a
weslyramalho <weslyramalho@yahoo.com.br>
2023-08-14 18:25:46 -0300

    remove

--- eventos/pkg/events/event_dispatcher_test.go
+++ eventos/pkg/events/event_dispatcher_test.go
@@ -116,6 +116,32 @@ func (suite *EventDispatcherTestSuite) TestEventDispatcher_Has() {
 
 }
 
+func (suite *EventDispatcherTestSuite) TestEventDispatcher_Remove() {
+	//1º evento
+	err := suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler)
+	suite.Nil(err)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+
+	err = suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler2)
+	suite.Nil(err)
+	suite.Equal(2, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+
+	//2º evento
+	err = suite.eventDispatcher.Register(suite.event2.GetName(), &suite.handler3)
+	suite.Nil(err)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event2.GetName()]))
+
+	suite.eventDispatcher.Remove(suite.event.GetName(), &suite.handler)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+	assert.Equal(suite.T(), &suite.handler2, suite.eventDispatcher.handlers[suite.event.GetName()][0])
+
+	suite.eventDispatcher.Remove(suite.event.GetName(), &suite.handler2)
+	suite.Equal(0, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+
+	suite.eventDispatcher.Remove(suite.event2.GetName(), &suite.handler3)
+	suite.Equal(0, len(suite.eventDispatcher.handlers[suite.event2.GetName()]))
+}
+
 type MockHandler struct {
 	mock.Mock
 }
