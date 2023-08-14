857202959763c21d856866759c6697f3f52c47c2
weslyramalho <weslyramalho@yahoo.com.br>
2023-08-14 20:18:25 -0300

    fila

---
+++ eventos/cmd/consumer/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/weslyramalho/GO/eventos/pkg/rabbitmq"
+)
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	msgs := make(chan amqp.Delivery)
+
+	go rabbitmq.Consume(ch, msgs)
+
+	for msg := range msgs {
+		fmt.Println(string(msg.Body))
+		msg.Ack(false)
+	}
+}
