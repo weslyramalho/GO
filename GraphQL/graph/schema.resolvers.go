8fa1fabacce27fa89d669551457beec241dcb8f1
weslyramalho <weslyramalho@yahoo.com.br>
2023-08-15 20:06:48 -0300

    query de category

--- GraphQL/graph/schema.resolvers.go
+++ GraphQL/graph/schema.resolvers.go
@@ -32,7 +32,20 @@ func (r *mutationResolver) CreateCourse(ctx context.Context, input model.NewCour
 
 // Categories is the resolver for the categories field.
 func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, error) {
-	panic(fmt.Errorf("not implemented: Categories - categories"))
+	categories, err := r.CategoryDB.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var categoriesModal []*model.Category
+	for _, category := range categories {
+		categoriesModal = append(categoriesModal, &model.Category{
+			ID:          category.ID,
+			Name:        category.Name,
+			Description: &category.Description,
+		})
+	}
+	return categoriesModal, nil
 }
 
 // Course is the resolver for the course field.
