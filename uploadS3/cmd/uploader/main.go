55a31dba7d04705fce993520c264c3af4aad289d
weslyramalho <weslyramalho@yahoo.com.br>
2023-08-18 16:51:58 -0300

    up

---
+++ uploadS3/cmd/uploader/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+var (
+	s3Client *s3.S3
+	s3Bucket string
+)
+
+func init() {
+	sess, err := session.NewSession(
+		&aws.Config{
+			Region: aws.String("us-east-1"),
+			Credentials: credentials.NewStaticCredentials(
+				"---",
+				"---",
+				"",
+			),
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+	s3Client = s3.New(sess)
+	s3Bucket = "goexpert-bucket-exemplo"
+}
+func main() {
+	dir, err := os.Open("./tmp")
+	if err != nil {
+		panic(err)
+	}
+	defer dir.Close()
+
+}
+
+func uploadFile(filename string) {
+	completeFileName := fmt.Sprintf("./tmp/%s", filename)
+	fmt.Printf("Uploading file %s to bucket %s", completeFileName, s3Bucket)
+	f, err := os.Open(completeFileName)
+	if err != nil {
+		fmt.Printf("Error opening file %s\n", completeFileName)
+		return
+	}
+	defer f.Close()
+	_, err = s3Client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(s3Bucket),
+		Key:    aws.String(filename),
+		Body:   f,
+	})
+	if err == nil {
+		fmt.Printf("Error uploading file %s\n", completeFileName)
+		return
+	}
+	fmt.Println("File %s uploaded sucessfully\n", completeFileName)
+}
