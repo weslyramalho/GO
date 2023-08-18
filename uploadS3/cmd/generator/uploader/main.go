1d49922b4a217d755e78af67f8ad15ad9bfd95b3
weslyramalho <weslyramalho@yahoo.com.br>
2023-08-18 16:30:06 -0300

    upload

---
+++ uploadS3/cmd/generator/uploader/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+var (
+	s3Client *s3.S3
+	s3Bucket string
+)
+
+func init() {
+	sess, err := session.NewSession(
+		&aws.Config{
+			Region: aws.String("us-east-1"),
+			Credentials: credentials.NewStaticCredentials(
+				"---",
+				"---",
+				"",
+			),
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+	s3Client = s3.New(sess)
+	s3Bucket = "goexpert-bucket-exemplo"
+}
+func main() {
+
+}
--- uploadS3/go.mod
+++ uploadS3/go.mod
@@ -1,3 +1,7 @@
 module github.com/weslyramalho/GO/uploads3
 
 go 1.19
+
+require github.com/aws/aws-sdk-go v1.44.327
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
