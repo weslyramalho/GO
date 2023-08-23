62139ec74e85a251b3249bf687953617b21dea9a
weslyramalho <weslyramalho@yahoo.com.br>
2023-08-23 16:46:32 -0300

    category repository

---
+++ UOW/internal/repository/category.go
@@ -0,0 +1,24 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/weslyramalho/GO/tree/main/UOW/internal/entity"
+)
+
+type CategoryRepositoryInterface interface {
+	Insert(ctx context.Context, category entity.Category) error
+}
+
+type CategoryRepository struct {
+	DB      *sql.DB
+	Queries *db.Queries
+}
+
+func NewCategoryRepository(dtb *sql.DB) *CategoryRepository {
+	return &CategoryRepository{
+		DB:      dtb,
+		Queries: db.New(dtb),
+	}
+}
